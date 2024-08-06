2da05f21f12c55ce8dba7d96711e24d12bc2c770
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-08-06 10:19:07 +0200

    replace a-tags in header with Link components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 export function Header(): ReactElement {
   return (
     <header className="header">
       <h1 className="logo">The Movie List</h1>
       <div className="links">
-        <a href="#" className="link">
-          Add new Movie
-        </a>
-        <a href="#" className="link">
+        <Link to="/" className="link">
           Movie List
-        </a>
+        </Link>
+        <Link to="/add-movie" className="link">
+          Add new Movie
+        </Link>
       </div>
     </header>
   );
